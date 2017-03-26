d6d8fb1bd5b7823872b7ce931309e2af47797421
Grainne Costigan <costigag@gmail.com>
2017-03-26 23:36:15 +0100

    Addition of each persons code

---
+++ NLTK_code/Text_as_fn_of_children.py
@@ -0,0 +1,277 @@
+
+# coding: utf-8
+
+# # Analysis of text fields as a function of having children or not. 
+# - Text is read in as a panda data frame. 
+# - It is then split dependent on whether the person has children or not. (There should be a better way to do this but I just used kids_present = survey[survey['children'] == 'Yes'] )
+# - 10 most common words, bigrams and trigrams are then found for children or no children selection. 
+# - See below for more information on how bigrams and trigrams are found. 
+
+# In[1]:
+
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import wordpunct_tokenize
+from nltk.stem.snowball import SnowballStemmer
+from nltk import FreqDist
+from nltk.collocations import *
+
+from autocorrect import spell
+from wordcloud import WordCloud
+from matplotlib import pyplot as plt
+import random
+
+
+#Function from Colm C. 
+def make_text_string(df, column_name):
+    
+    
+    stop_words = set(stopwords.words('english'))
+    stemmer = SnowballStemmer("english")
+    
+    stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
+    
+    #remove stop words and punctuation from text. Make into one long string. 
+    #reduce words to their 'stem' so we do not get repetitions of same form  i.e. say and saying. 
+    word_list = [stemmer.stem(i.lower()) for i in wordpunct_tokenize(" ".join(df[column_name].dropna())) if i.lower() not in stop_words]
+    word_list = [spell(i) for i in word_list]
+    workable_text = ' '.join(word_list)
+        
+    return workable_text
+
+# Ten most common words are found given table and column name. 
+def Ten_most_common(table,column_name):
+    #Make a full string of text column
+    field_txt = make_text_string(table,column_name)
+    #Split into words. 
+    field_Tkn = nltk.tokenize.word_tokenize(field_txt)
+    #Find frequency distribution
+    fdist_field = FreqDist(field_Tkn)
+    #return top 10 words. 
+    return fdist_field.most_common(10)
+
+
+# Here we are searching for bigrams and trigrams. 
+# 
+# I apply a frequency filter to show only those bigrams that appear more than 3 times in the text.
+# WRT the measures. PMI measures the association of two words by calculating the log ( p(x|y) / p(x) ), so it's not only about the frequency of a word occurrence or a set of words concurring together. To achieve high PMI, you need both:
+# High p(x|y)
+# low p(x)
+# So unusual words occuring with the same word will get a higher rank than other bigrams.
+# 
+# Note: I found it useful to look at the scores of the bigrams and trigrams to understand what was going on. 
+# 
+# 
+
+# In[16]:
+
+def Field_bigrams(table,column_name,filter_freq,no2return):
+    #A pandas dataframe is given with the column name, the filter frequency to be applied to the bigram or trigram, and the number of results to be returned. 
+    #The field is converted to a string, and tokenized before being passed ot the bigram fn. 
+    
+    field_txt = make_text_string(table,column_name)
+    field_Tkn = nltk.tokenize.word_tokenize(field_txt)
+    
+    bigram_measures = nltk.collocations.BigramAssocMeasures()
+    finder = BigramCollocationFinder.from_words(field_Tkn)
+
+    #Words can be highly collocated but the expressions are also very infrequent. 
+    #Therefore it is useful to apply filters, such as ignoring all bigrams which occur 
+    #less than three times in the corpus:
+    finder.apply_freq_filter(filter_freq)
+    return finder.nbest(bigram_measures.pmi, no2return)
+
+
+def Field_trigrams(table,column_name,filter_freq, no2return):
+    field_txt = make_text_string(table,column_name)
+    field_Tkn = nltk.tokenize.word_tokenize(field_txt)
+    
+    trigram_measures = nltk.collocations.TrigramAssocMeasures()
+    finder = TrigramCollocationFinder.from_words(field_Tkn)
+    
+    #A different way to filter the results for how often they occur. 
+    #scored = finder.score_ngrams(trigram_measures.raw_freq)
+    #set(trigram for trigram, score in scored) == set(nltk.trigrams(field_Tkn))
+    #return sorted(finder.nbest(trigram_measures.raw_freq,filter_freq))
+    
+    finder.apply_freq_filter(filter_freq)
+    return finder.nbest(trigram_measures.pmi, no2return)
+
+
+# In[17]:
+
+## Read in survey results. 
+fn = 'survey_results_clean.csv.xlsx'
+#survey = pd.read_excel(fn, delimiter = '\t')
+survey = pd.read_excel(fn)
+
+
+# In[18]:
+
+## Seperating those with and without kids. 
+## There should be a better way to do this: pandas groupby
+kids_present = survey[survey['children'] == 'Yes']
+print('No people with kids',len(kids_present))
+no_kids = survey[survey['children'] == 'No']
+print('No of people without kids',len(no_kids))
+
+
+# In[19]:
+
+#Return the top 10 words within this text field
+Ten_most_common(kids_present,'improve_contact_family_other_suggestions')
+
+
+# In[20]:
+
+# Searcing for bigrams but ignoring all bigrams which occur less than 3 times in the corpus:
+Field_bigrams(kids_present,'improve_contact_family_other_suggestions',3,10)
+
+
+# In[9]:
+
+# Searcing for bigrams but ignoring all bigrams which occur less than 5 times in the corpus:
+Field_bigrams(kids_present,'improve_contact_family_other_suggestions',5,5)
+
+#Check context of some words....
+#noK_improve_txt.concordance('fail')
+#noK_improve_txt.concordance('meal')
+
+# It was useful for me to look at the scorings of these bigrams to see how they were working. 
+# scored = finder.score_ngrams(bigram_measures.raw_freq)
+# set(trigram for trigram, score in scored) == set(nltk.trigrams(K_improve_Tkn))
+# select only the top n results:
+
+
+# In[38]:
+
+#Showing the top three trigrams for this text field. 
+Field_trigrams(kids_present,'improve_contact_family_other_suggestions',6,3)
+
+
+# In[10]:
+
+#Return the top 10 words within this text field
+Ten_most_common(no_kids,'improve_contact_family_other_suggestions')
+
+
+# In[41]:
+
+#Showing the top three trigrams for this text field. 
+Field_trigrams(no_kids,'improve_contact_family_other_suggestions',3,3)
+
+
+# Analysis of *prison_service_facilities_other_thoughts* **with** kids. 
+
+# In[43]:
+
+#Top 10 most common words. 
+Ten_most_common(kids_present,'prison_service_facilities_other_thoughts')
+
+
+# In[44]:
+
+####ignoring all bigrams which occur less than 3 times in the corpus:
+Field_bigrams(kids_present,'prison_service_facilities_other_thoughts',3,10)
+
+
+# In[45]:
+
+####ignoring all bigrams which occur less than 5 times in the corpus:
+Field_bigrams(kids_present,'prison_service_facilities_other_thoughts',5,5)
+
+
+# In[47]:
+
+#Showing the top three trigrams for this text field. 
+Field_trigrams(kids_present,'prison_service_facilities_other_thoughts',6,3)
+
+
+# Analysis of *"prison_service_facilities_other_thoughts"* **without kids**
+
+# In[48]:
+
+#Top 10 most common words. 
+Ten_most_common(no_kids,'prison_service_facilities_other_thoughts')
+
+
+# In[49]:
+
+####ignoring all bigrams which occur less than 3 times in the corpus:
+Field_bigrams(no_kids,'prison_service_facilities_other_thoughts',3,10)
+
+
+# In[50]:
+
+####ignoring all bigrams which occur less than 5 times in the corpus:
+Field_bigrams(no_kids,'prison_service_facilities_other_thoughts',5,5)
+
+
+# In[54]:
+
+#Showing the top three trigrams for this text field. 
+Field_trigrams(no_kids,'prison_service_facilities_other_thoughts',3,3)
+
+
+# ----------------------------------------------------------------
+# See if there are any changes when you select by childrens age. 
+
+# In[66]:
+
+adult_kids = survey[survey['adult_children'] == 0]
+print('No people with adult kids',len(adult_kids))
+
+
+# Only 4 people have adult children. Not much point in looking for differences. 
+
+# ----------------------------------------------------------------
+# See where the empty fields lie. 
+
+# In[81]:
+
+empty_fields = survey['prison_service_facilities_other_thoughts'].isnull()
+
+#Need to look further into how to manipulate pandas tables in this way. 
+
+
+# In[ ]:
+
+
+
+
+# # Test on Irene's code. 
+
+# In[10]:
+
+survey['thoughts_facts'] = survey['prison_service_facilities_other_thoughts'] + survey['improve_contact_family_other_suggestions']
+
+
+# In[11]:
+
+#survey['thoughts_facts'].most_common()
+Ten_most_common(survey,'thoughts_facts')
+
+
+# In[12]:
+
+Field_bigrams(survey,'thoughts_facts',3,10)
+
+
+# In[ ]:
+
+sentence_ngrams = {}
+stop_list = stopwords.words('english') + list(string.punctuation)
+groups = ['prison_service_facilities_other_thoughts', 'improve_contact_family_other_suggestions']
+
+for col in ['sentence_length', 'age', 'prison_wing_main']:
+    temp = {}
+    for idx, grp in df.groupby(col):
+        words1 = grp[pd.notnull(grp['thoughts_facs'])]['thoughts_facs'].tolist()
+        w1 = ' '.join(words1)
+        all_tokens = [t for t in word_tokenize(w1)]
+        tokens_no_stop = lower_no_stops(all_tokens)
+        temp[idx]= Counter(ngrams(stem_all(all_tokens), 2)).most_common(5)
+    
+    sentence_ngrams[col] = temp
+
---
+++ NLTK_code/final_sprint1_nlp.py
@@ -0,0 +1,290 @@
+
+# coding: utf-8
+
+# In[4]:
+
+import pandas as pd
+import numpy as np
+# import nltk
+# from nltk.tokenize import word_tokenize
+# from collections import Counter
+# from nltk.corpus import stopwords
+# import string
+# from nltk.stem import PorterStemmer
+# from nltk import ngrams
+import seaborn as sns
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.tokenize import word_tokenize
+from collections import Counter
+from nltk.corpus import stopwords
+import string
+from nltk.stem import PorterStemmer
+from nltk import ngrams
+
+get_ipython().magic('matplotlib inline')
+
+
+# In[5]:
+
+
+
+
+
+# In[7]:
+
+df = pd.read_csv('survey_results_clean.csv', na_values=['nan'])
+
+
+# Note: Population distribution is not normal, therefore certain groups will appear as having a stronger signal than others. It might be a good idea to correct this if each age group is to be considered equal. Alternatively, if the majority of the population is to be considered only, then can leave the data as is.
+# 
+
+# In[8]:
+
+df['age'].value_counts()
+
+
+# The results for improving family visits and facilities seem to suggest the same common theme. First, run each column individually, then combine the two.
+
+# ### Binary Fields:
+# 
+# Count the filled in binary fields.
+
+# #### Family contact: 
+
+# In[9]:
+
+contact_cols = [col for col in df.columns if 'improve_contact_family' in col] #find all the contact family cols
+contact_cols.extend(['age', 'sentence_length', 'prison_wing_main']) # interested in breaking out the results using these cols
+contact_clean_data = df[contact_cols].fillna(0)
+
+
+# In[6]:
+
+#split_cols = to_plot.columns.map(lambda x: x.split('_')[3:])
+
+
+# Mean normalize:
+
+# In[7]:
+
+# Leaving this out as it might not be suitable to give equal weight to each population group.
+#pop_max = df.groupby('age')['Unnamed: 0'].count().max()
+#pop_min = df.groupby('age')['Unnamed: 0'].count().min()
+
+
+# By Age:
+
+# In[10]:
+
+#tp_scaled = to_plot.T.apply(lambda x: (x - pop_min)/(pop_max - pop_min))
+
+to_plot = contact_clean_data.groupby('age').sum()
+
+sns.heatmap(to_plot.T)
+
+
+# By sentence length:
+
+# In[11]:
+
+to_plot = contact_clean_data.groupby('sentence_length').sum()
+
+sns.heatmap(to_plot.T)
+
+
+# By prison wing:
+
+# In[12]:
+
+to_plot = contact_clean_data.groupby('prison_wing_main').sum()
+
+sns.heatmap(to_plot.T)
+
+
+#  #### Facilities improvement binary fields
+
+# In[13]:
+
+fac_cols = [col for col in df.columns if 'prison_service_facilities' in col]
+fac_cols.extend(['age', 'sentence_length', 'prison_wing_main'])
+fac_clean_data = df[fac_cols].fillna(0)
+
+
+# By age:
+
+# In[14]:
+
+to_plot_fac = fac_clean_data.groupby('age').sum()
+tp_fac = to_plot_fac.copy()
+#tp_fac = tp_fac.T.apply(lambda x: (x - pop_min)/(pop_max - pop_min))
+
+sns.heatmap(tp_fac.T)
+
+
+# By sentence length:
+
+# In[15]:
+
+to_plot_fac = fac_clean_data.groupby('sentence_length').sum()
+tp_fac = to_plot_fac.copy()
+#tp_fac = tp_fac.T.apply(lambda x: (x - pop_min)/(pop_max - pop_min))
+
+sns.heatmap(tp_fac.T)
+
+
+# By prison wing:
+
+# In[14]:
+
+to_plot_fac = fac_clean_data.groupby('prison_wing_main').sum()
+tp_fac = to_plot_fac.copy()
+#tp_fac = tp_fac.T.apply(lambda x: (x - pop_min)/(pop_max - pop_min))
+
+sns.heatmap(tp_fac.T)
+
+
+# ### Free Text Analysis:
+
+# Combine the free text thought fields, these yield similar results individually. Combined together they can send a stronger message for the desired improvements within the facility.
+
+# In[16]:
+
+df['thoughts_facs'] = df['prison_service_facilities_other_thoughts'] + df['improve_contact_family_other_suggestions']
+
+
+# In[19]:
+
+sentence_ngrams = {}
+stop_list = stopwords.words('english') + list(string.punctuation)
+groups = ['prison_service_facilities_other_thoughts', 'improve_contact_family_other_suggestions']
+
+for col in ['sentence_length', 'age', 'prison_wing_main']:
+    temp = {}
+    for idx, grp in df.groupby(col):
+        words1 = grp[pd.notnull(grp['thoughts_facs'])]['thoughts_facs'].tolist()
+        w1 = ' '.join(words1)
+        all_tokens = [t for t in word_tokenize(w1)]
+        tokens_no_stop = lower_no_stops(all_tokens)
+        temp[idx]= Counter(ngrams(stem_all(all_tokens), 2)).most_common(5)
+    
+    sentence_ngrams[col] = temp
+
+
+# In[18]:
+
+pd.DataFrame(sentence_ngrams['sentence_length'])
+
+
+# In[18]:
+
+pd.DataFrame(sentence_ngrams['age'])
+
+
+# In[19]:
+
+# need to remove wing J as it has no free text values
+sentence_ngrams['prison_wing_main'].pop('J')
+
+
+# In[20]:
+
+pd.DataFrame(sentence_ngrams['prison_wing_main'])
+
+
+# Regardless of any age, sentence or prison wing detail, it appears there is a theme for desired improvements:
+
+# In[21]:
+
+words1 = df[pd.notnull(df['thoughts_facs'])]['thoughts_facs'].tolist()
+w1 = ' '.join(words1)
+all_tokens = [t for t in word_tokenize(w1)]
+tokens_no_stop = lower_no_stops(all_tokens)
+Counter(ngrams(stem_all(all_tokens), 2)).most_common(10)
+
+
+# #### Misc work
+
+# Is there a connection between whent he survey was taken and the responses?
+
+# In[22]:
+
+df['survey_time'] = pd.to_datetime(df['survey_time'])
+clean_data_time = df.set_index('survey_time')
+
+
+# Break out by day:
+
+# In[23]:
+
+by_day = {}
+for idx, grp in clean_data_time.groupby(pd.Grouper(freq='D')):
+    print idx, len(grp)
+    words1 = grp[pd.notnull(grp['thoughts_facs'])]['thoughts_facs'].tolist()
+    w1 = ' '.join(words1)
+    all_tokens = [t for t in word_tokenize(w1)]
+    tokens_no_stop = lower_no_stops(all_tokens)
+    by_day[idx]= Counter(ngrams(stem_all(all_tokens), 2)).most_common(5)
+
+
+
+# Potentially should not be surprising to see common responses on common days. 
+
+# In[24]:
+
+count_nulls = df.replace(np.NaN, -1)
+
+
+# Count the number of nan's per age group
+
+# In[25]:
+
+print groups[0]
+count_nulls[count_nulls[groups[0]] == -1].groupby('age')[groups[0]].count()
+
+
+# In[26]:
+
+
+print groups[1]
+count_nulls[count_nulls[groups[1]] == -1].groupby('age')[groups[1]].count()
+
+
+# Can do the above for sentence length, prison wing
+
+# In[27]:
+
+df['children_number'].value_counts()
+
+
+# In[28]:
+
+print groups[1]
+count_nulls[count_nulls[groups[1]] == -1].groupby('children_number')[groups[1]].count()
+
+
+# Does not look like there is any connection between the number of children and leaving free text comments in the thoughts fields.
+
+# In[29]:
+
+adf = df['age'].value_counts()
+fig, ax = plt.subplots(figsize=(10,8))
+#ax.set_facecolor('w')
+ax.grid(color=(0.5,0.5,0.5),lw=0.25)
+# df['age'].value_counts().plot(kind='bar')
+# plt.plot(x=df['age']data=df['age'].value_counts)
+x_pos = np.arange(len(adf.index))
+ax.bar(x_pos, adf.values, width=0.25)
+
+ax.set_xticks(x_pos)
+ax.set_xticklabels(adf.index)
+print
+
+
+
+# In[ ]:
+
+
+
