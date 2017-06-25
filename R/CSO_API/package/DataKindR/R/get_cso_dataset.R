deaaaafb2b580343f9e4896ffb7734b9bc9cab63
cormac85 <cormac85@gmail.com>
2017-06-25 19:47:26 +0100

    Initial commit to create DataKindR package. Initially this will contain the files for accessing the CSO API

---
+++ R/CSO_API/package/DataKindR/R/get_cso_dataset.R
@@ -0,0 +1,10 @@
+library(rjstat)
+
+get_cso_dataset <- function(cso_dataset_code){
+  cso_base_url <- 
+    'http://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'  
+  
+  fromJSONstat(readLines(paste0(cso_base_url, cso_dataset_code)))
+  
+}
+
