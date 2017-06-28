54b3d623a88f6e511e9540324f219c6745b615d8
cormac85 <cormac85@gmail.com>
2017-06-28 22:27:09 +0100

    Removing old file versions

--- R/CSO_API/package/old files/get_cso_dataset.R
+++
@@ -1,9 +0,0 @@
-
-get_cso_dataset <- function(cso_dataset_code){
-  cso_base_url <-
-    'http://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
-
-  rjstat::fromJSONstat(readLines(paste0(cso_base_url, cso_dataset_code)))
-
-}
-
